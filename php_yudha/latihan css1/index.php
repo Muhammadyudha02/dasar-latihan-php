2ee20dba413a633247ca6cb26b76a301d0f0c920
Muhammadyudha02 <127858233+Muhammadyudha02@users.noreply.github.com>
2024-05-06 15:42:09 +0700

    Add files via upload

---
+++ php_yudha/latihan css1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>glow shadow bottom</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <a href="a" class="btn">Login</a>
+    <br>
+    <a href="b" class="tombol">masuk</a>
+    <a href="c" class="text">kembali</a>
+</body>
+</html>
